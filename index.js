081c0da997f3e20c2fa9292da9c3ee0ab9d4d142
Alexis Lopez <lopez111alex@gmail.com>
2024-01-16 01:21:08 -0800

    updated index.js

--- index.js
+++ index.js
@@ -1,34 +1,78 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Shapes } = require("./lib/shapes");
+const generateSVG = require("./lib/generateSVG");
+const colorNames = require("color-name-list");
+
+const validateColor = (input) => {
+  const hexColorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+  const validCSSColorNames = colorNames.map((color) =>
+    color.name.toLowerCase()
+  );
+
+  return (
+    validCSSColorNames.includes(input.toLowerCase()) ||
+    hexColorRegex.test(input.toLowerCase()) ||
+    "Please enter a valid color."
+  );
+};
 
 inquirer
-    .prompt([
-        {
-            type: 'input' ,
-            message: 'Enter up to 3 characters for your logo: ' ,
-            name: 'text',
-        },
-        {
-            type: 'input',
-            message: 'Select a color for your text: ',
-            name: 'textColor',
-        },
-        {
-            type:'list',
-            message: 'choose a shape: ',
-            name: 'shape',
-            choices: [
-                'Circle',
-                'Triangle',
-                'Square',
-            ]
-        },
-        {
-            type: 'input',
-            message: 'Select a color for your shape: ',
-            name: 'shapeColor',
-        },
-    ])
-    .then((response) => {
-        console.log(` You selected ${response.text} to be ur text and a ${response.shapeColor} ${response.shape} to be your shape.`);
-    });
+  .prompt([
+    {
+      type: "input",
+      name: "characters",
+      message: "Enter up to three characters.",
+      validate: (input) => input.length <= 3,
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Enter text color (CSS or hex):",
+      validate: validateColor,
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose a shape:",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      type: "input",
+      name: "shapecolor",
+      message: "Enter a shape color",
+      validate: validateColor,
+    },
+  ])
+  .then((response) => {
+    const { shape, shapecolor, ...data } = response;
+    const { triangle, circle, square } = Shapes({ shapecolor });
+    let SVGContent = "";
+
+    switch (shape) {
+      case "triangle":
+        SVGContent = generateSVG(data, triangle);
+        break;
+      case "circle":
+        SVGContent = generateSVG(data, circle);
+        break;
+      case "square":
+        SVGContent = generateSVG(data, square);
+        break;
+      default:
+        console.log("Invalid shape specified in data.");
+        return;
+    }
+
+    writeToFile("logo.svg", SVGContent);
+  });
+
+function writeToFile(fileName, SVGContent) {
+  fs.writeFile(fileName, SVGContent, (error) => {
+    if (error) {
+      console.log("Error has occurred!");
+    } else {
+      console.log("Generated logo.svg");
+    }
+  });
+}
